82f16802bcb7160ded28062781a727d951bcf4a7
Rupendra Bandyopadhyay <rbandyopadhyay@i-360.com>
2025-09-20 21:27:22 -0400

    End of section 3 course 1

--- edhek_risk_kit.py
+++ edhek_risk_kit.py
@@ -440,3 +440,56 @@ def summary_stats(r, riskfree_rate=0.03):
         "Sharpe Ratio": ann_sr,
         "Max Drawdown": dd
     })
+
+def gbm(n_years = 10, n_scenarios=1000, mu=0.07, sigma=0.15, steps_per_year=12, s_0=100.0, prices=True):
+    """
+    Evolution of Geometric Brownian Motion trajectories, such as for Stock Prices through Monte Carlo
+    :param n_years:  The number of years to generate data for
+    :param n_paths: The number of scenarios/trajectories
+    :param mu: Annualized Drift, e.g. Market Return
+    :param sigma: Annualized Volatility
+    :param steps_per_year: granularity of the simulation
+    :param s_0: initial value
+    :return: a numpy array of n_paths columns and n_years*steps_per_year rows
+    """
+    # Derive per-step Model Parameters from User Specifications
+    dt = 1/steps_per_year
+    n_steps = int(n_years*steps_per_year) + 1
+    # the standard way ...
+    # rets_plus_1 = np.random.normal(loc=mu*dt+1, scale=sigma*np.sqrt(dt), size=(n_steps, n_scenarios))
+    # without discretization error ...
+    rets_plus_1 = np.random.normal(loc=1+mu*dt, scale=(sigma*np.sqrt(dt)), size=(n_steps, n_scenarios))
+    rets_plus_1[0] = 1
+    ret_val = s_0*pd.DataFrame(rets_plus_1).cumprod() if prices else rets_plus_1-1
+    return ret_val
+    
+_rng = np.random.default_rng()
+
+def gbm_fast(n_years: int=10, 
+             n_scenarios: int=1000, 
+             mu: float=0.07, 
+             sigma: float=0.15,
+             prices: bool = False,
+             steps_per_year: int=12, 
+             s_0: float=100.0,
+             rng=_rng,
+             dtype=np.float32) -> pd.DataFrame | np.ndarray:
+    
+    dt   = 1.0 / steps_per_year
+    n    = int(n_years * steps_per_year)
+    sqdt = np.sqrt(dt)
+
+    # generate shocks directly in target dtype
+    xi   = rng.standard_normal((n+1, n_scenarios)).astype(dtype)
+
+    # convert constants to dtype explicitly
+    drift = np.array(1 + mu*dt, dtype=dtype)
+    vol   = np.array(sigma*sqdt, dtype=dtype)
+
+    inc  = drift + vol * xi
+    inc[0] = 1
+    if prices:
+        ret_val = s_0 * np.cumprod(inc, axis=0, dtype=dtype)
+        return pd.DataFrame(ret_val)
+    else:
+        return inc - 1   
